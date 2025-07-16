0e6a3d9c647f22f15bc25841bd4fae28072bbeeb
Your Name <your.email@example.com>
2025-07-16 16:57:28 +0900

    프롬프트 모듈화 이후

--- project.py
+++ project.py
@@ -3,6 +3,7 @@ import time
 import uuid
 import streamlit as st
 
+from prompt import prompt1, prompt2
 from setting import load_pdf, embed,search
 from dotenv import load_dotenv
 from langchain_upstage import ChatUpstage
@@ -35,8 +36,7 @@ def reset_chat():
 
 chat = ChatUpstage(upstage_api_key=api_key) #chat_bot 생성
 
-contextualize_q_system_prompt = """사용자에게 받은 질문을 해석해서 지금 사용자가 어떤걸 선호하는지, 기분은 어떠한지를
-차근차근 분석해 풀어써서 질문을 세세하게 답변하기 쉽게 재구성 시켜주세요."""
+contextualize_q_system_prompt = prompt1
 
 contextualize_q_prompt = ChatPromptTemplate.from_messages(
     [
@@ -50,20 +50,7 @@ history_aware_retriever = create_history_aware_retriever(
     chat, retriever, contextualize_q_prompt
 )
 
-qa_system_prompt = """당신은 한국의 전통 음식을 따뜻하고 친근하게 소개해주는 친절한 안내자 입니다. 당신의 목표는 사용자가 요청한 한국의 전통 음식을 알차게 설명하는 것입니다.
-**반드시, 서론이나 인삿말을 한 후 첫 번째 음식 소개를 시작하고, 답변의 마무리에는 조언이나 인사말로 마무리 해주세요.**
-
-사용자의 선호도나 기분은 다음과 같습니다:
-{context}
-
-다음 기준을 반드시 고려해 주세요:
-- 추천할 한국 전통 음식은 **4~7가지**로 제한합니다. (사용자가 특정 지역/상황을 명시하지 않으면 보편적인 전통 음식을 추천합니다.)
-- 각 음식의 **특징, 맛, 간단한 유래 또는 배경**을 설명하여 흥미를 유발합니다.
-- 중복되는 음식이 있으면 하나만 설명해주세요.
-- (선택 사항) 만약 해당 음식에 대한 사용자 기분 정보(context)가 있다면, 그에 맞춰 설명을 조절합니다.
-
-**또한, 각 음식 설명 사이에는 반드시 구분선(---)을 넣어주세요.**
-"""
+qa_system_prompt =prompt2
 qa_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", qa_system_prompt),
